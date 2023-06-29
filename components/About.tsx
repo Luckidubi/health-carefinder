535dcbf1f3c4cb9574ab43a4ad9c9b32df45a92b
Dubem Nwodo <nwodochidubem1@gmail.com>
2023-06-29 23:38:25 +0400

    feat: Add About section

---
+++ components/About.tsx
@@ -0,0 +1,39 @@
+import { ArrowRight } from "lucide-react";
+import Image from "next/image";
+import React from "react";
+import { Button } from "./ui/button";
+
+const About = () => {
+  return (
+    <section className="flex flex-col xl:flex-row gap-4 md:pt-20">
+      <div className="flex-[1.5] w-[80%]">
+        <Image
+          src="/Group 77.png"
+          alt="About"
+          width={600}
+          height={900}
+          className="object-contain"
+        />
+      </div>
+
+      <div className="flex-1  w-[full]">
+        <h2 className="hero__title pb-5 lg:text-[50px] tracking-wider text-[30px] font-semibold">
+          Welcome to CareFinder{" "}
+        </h2>
+        <p className="hero__subtitle pb-5">
+          Carefinder is a platform where users can search for hosiptals in their
+          areas, export hospital details for your records and enhance your
+          healthcare experience by connecting with others and sharing valuable
+          resources.
+        </p>
+
+        <Button className="bg-blue-900 hover:bg-blue-500 rounded-lg text-white py-8 xl:px-12 xl:text-2xl text-lg font-bold my-4">
+         OUR SERVICES
+          <ArrowRight className="ml-4 lg:h-8 w-12" />
+        </Button>
+      </div>
+    </section>
+  );
+};
+
+export default About;
--- tailwind.config.js
+++ tailwind.config.js
@@ -34,6 +34,10 @@ module.exports = {
         "accordion-down": "accordion-down 0.2s ease-out",
         "accordion-up": "accordion-up 0.2s ease-out",
       },
+      backgroundImage: {
+
+        'hero-bg': "url('/Rectangle 1.png')"
+      }
     },
   },
   plugins: [require("tailwindcss-animate")],
