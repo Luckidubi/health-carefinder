b67492efe04ac29fbe3cb890d145931f9423db63
Dubem Nwodo <nwodochidubem1@gmail.com>
2023-06-29 23:40:32 +0400

    chore: Add Hero, About section to Home

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,23 @@
-import Hero from '@/components/Hero'
-import Image from 'next/image'
+import About from "@/components/About";
+import Hero from "@/components/Hero";
+import SearchInput from "@/components/SearchInput";
+import Image from "next/image";
 
 export default function Home() {
   return (
     <main className="overflow-hidden">
-<Hero/>
+      <Hero />
+      <div className="padding-x flex flex-col flex-center bg-gray-200">
+        <h3 className=" text-center text-blue-900 text-[25px] font-medium leading-9 pb-4">
+          Find a nearby hospital
+        </h3>
+        <div className="lg:w-[688px] w-3/4 h-16 rounded-[25px] pb-10">
+          <SearchInput />
+        </div>
+      </div>
+      <div id="#About" className="padding-x padding-y bg-white max-width">
+        <About />
+      </div>
     </main>
-  )
+  );
 }
